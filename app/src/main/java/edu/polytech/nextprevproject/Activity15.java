e594c8d7cda671f24deba7bad7203112bf1a7844
노윤호 <shdbsghxg@gmail.com>
2019-06-17 15:46:49 +0900

    [Activity15] add prev, next button.

--- app/src/main/java/edu/polytech/nextprevproject/Activity15.java
+++ app/src/main/java/edu/polytech/nextprevproject/Activity15.java
@@ -1,6 +1,34 @@
 package edu.polytech.nextprevproject;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity15  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity15);
+        Button prev = (Button)findViewById(R.id.pre_15);
+        Button next = (Button)findViewById(R.id.next_15);
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity14.class);
+                startActivity(intent);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity16.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
