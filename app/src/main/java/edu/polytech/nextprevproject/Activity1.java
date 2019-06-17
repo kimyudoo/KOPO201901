6c97935aa5bc4b889ba6af1749d6d25923f1253c
leegiljoon <leegiljoon@gmail.com>
2019-06-17 15:45:23 +0900

    add Activity1 button

--- app/src/main/java/edu/polytech/nextprevproject/Activity1.java
+++ app/src/main/java/edu/polytech/nextprevproject/Activity1.java
@@ -1,6 +1,36 @@
 package edu.polytech.nextprevproject;
 
+import android.content.Intent;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Activity1 extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button activity1Button1, activity1Button2;
+        setContentView(R.layout.activity1);
+        activity1Button1 = (Button) findViewById(R.id.activity1Button1);
+        activity1Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        activity1Button2 = (Button) findViewById(R.id.activity1Button2);
+        activity1Button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(getApplicationContext(), Activity2.class);
+                startActivity(intent1);
+
+            }
+
+
+        });
+    }
 }
